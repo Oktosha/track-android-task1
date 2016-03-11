c9c7dd4261a4bff3444bc75ab8d163186ad8b75b
Daria Kolodzey <kolodaria@google.com>
2016-03-11 10:04:23 +0300

    Add sleep to First activity

--- app/src/main/java/dkolodzey/tracktask1/First.java
+++ app/src/main/java/dkolodzey/tracktask1/First.java
@@ -1,5 +1,6 @@
 package dkolodzey.tracktask1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,18 @@ public class First extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        try {
+            Thread.sleep(2000);
+            Intent intent = new Intent(this, Second.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            //Ignore
+        }
+
     }
 }
